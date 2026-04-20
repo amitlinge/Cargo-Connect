cd26a1772a6d22dd259cb308a1fe611ead1e6160
amitkumar linge <amitlinge410@gmail.com>
2026-04-20 15:10:30 +0530

    ..

--- CargoConnect/CargoConnect.API/Program.cs
+++ CargoConnect/CargoConnect.API/Program.cs
@@ -3,7 +3,6 @@ using CargoConnect.Application.Infrastructure.RegisterServices;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//new line
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
