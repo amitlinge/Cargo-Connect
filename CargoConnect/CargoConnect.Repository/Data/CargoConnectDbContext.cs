1f7e737197edfcaa7e51e7e2f7feb666903d732e
amitkumar linge <amitlinge410@gmail.com>
2026-04-20 12:35:51 +0530

    unique key constraint

--- CargoConnect/CargoConnect.Repository/Data/CargoConnectDbContext.cs
+++ CargoConnect/CargoConnect.Repository/Data/CargoConnectDbContext.cs
@@ -14,9 +14,17 @@ namespace CargoConnect.Repository.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserEntity>().HasIndex(u => u.Email).IsUnique();
+            modelBuilder.Entity<DriverEntity>().HasIndex(d => d.Email).IsUnique();
+            modelBuilder.Entity<DriverEntity>().HasIndex(d => d.LicenseNumber).IsUnique();
+            modelBuilder.Entity<VehicalEntity>().HasIndex(d => d.NumberPlate).IsUnique();
+        }
         public DbSet<UserEntity> users { get; set; }
         public DbSet<DriverEntity> drivers{ get; set; }
-
+        public DbSet<VehicalEntity> vehicals { get; set; }
         public DbSet<TrackingEntity> trackings { get; set; }
         public DbSet<PaymentEntity> payments { get; set; }
         public DbSet<BookingEntity> bookings { get; set; }
