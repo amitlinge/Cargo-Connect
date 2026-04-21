107d8c02851302ac6df72fec1d46e67233c6b59b
mugdhakashid <mugdhakashid@gmail.com>
2026-04-21 20:19:23 +0530

    create driver

---
+++ CargoConnect/CargoConnect.Application/DTOs/Driver/DriverCreateDTO.cs
@@ -0,0 +1,12 @@
+﻿public class DriverCreateDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+    public string Phone { get; set; }
+
+    public string Password { get; set; }
+
+    public string LicenseNumber { get; set; }
+}
