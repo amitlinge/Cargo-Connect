45aac49290775abeebe0e997e86ef97b8c9eb28b
amitkumar linge <amitlinge410@gmail.com>
2026-04-22 11:33:55 +0530

    added service of automapper

--- CargoConnect/CargoConnect.API/Program.cs
+++ CargoConnect/CargoConnect.API/Program.cs
@@ -1,4 +1,5 @@
 using CargoConnect.Application.Infrastructure.RegisterServices;
+using CargoConnect.Application.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile<ApplicationMappingProfile>();
+});
 
 builder.Services.AddApplicationServices(builder.Configuration);
 
