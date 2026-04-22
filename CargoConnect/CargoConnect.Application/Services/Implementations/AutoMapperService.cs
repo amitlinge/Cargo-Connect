b3c3e8f67349e07956dcfdf35b97f7fd739b73f5
amitkumar linge <amitlinge410@gmail.com>
2026-04-22 11:23:38 +0530

    automapper service

---
+++ CargoConnect/CargoConnect.Application/Services/Implementations/AutoMapperService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CargoConnect.Application.Services.Interfaces;
+
+namespace CargoConnect.Application.Services.Implementations
+{
+    public class AutoMapperService : IMappingService
+    {
+        IMapper _mapper;
+
+        public AutoMapperService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public TDest Map<TSource, TDest>(TSource source)
+        {
+            return _mapper.Map<TSource, TDest>(source);
+        }
+
+        public List<TDest> MapList<TSource, TDest>(List<TSource> sources)
+        {
+            return _mapper.Map<List<TDest>>(sources);
+        }
+    }
+}
