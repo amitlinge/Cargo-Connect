1d447786f49d68584481c5f20d10fb390dbb6d6a
Ankita Khade <ankita24.net@gmail.com>
2026-04-22 13:08:03 +0530

    Added DriverUpdateDTO Validations

--- CargoConnect/CargoConnect.Application/DTOs/Driver/DriverUpdateDTO.cs
+++ CargoConnect/CargoConnect.Application/DTOs/Driver/DriverUpdateDTO.cs
@@ -1,11 +1,23 @@
-﻿public class DriverUpdateDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+public class DriverUpdateDTO
 {
+    [Required(ErrorMessage = "Id is required")]
+    [Range(1, int.MaxValue, ErrorMessage = "Invalid Id")]
     public int Id { get; set; }
 
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    [MinLength(2, ErrorMessage = "First name must be at least 2 characters")]
+    [MaxLength(50)]
+    public string? FirstName { get; set; }
 
-    public string Phone { get; set; }
+    [MinLength(2, ErrorMessage = "Last name must be at least 2 characters")]
+    [MaxLength(50)]
+    public string? LastName { get; set; }
 
-    public string LicenseNumber { get; set; }
+    [RegularExpression(@"^[6-9]\d{9}$", ErrorMessage = "Invalid phone number")]
+    public string? Phone { get; set; }
+
+    [MinLength(5)]
+    [MaxLength(20)]
+    public string? LicenseNumber { get; set; }
 }
