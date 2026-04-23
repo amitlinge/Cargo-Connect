ab12589339b0e14bac46865dde2c3a49549599d6
mugdhakashid <mugdhakashid@gmail.com>
2026-04-23 20:18:29 +0530

    Driver View Model

---
+++ CargoConnect/CargoConnect.MVC.Client/Models/Driver/DriverViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CargoConnect.Common.Enums;
+
+namespace CargoConnect.MVC.Client.Models.Driver
+{
+    public class DriverViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public string LicenseNumber { get; set; }
+
+        public DriverStatus Status { get; set; }
+
+        public float? Rating { get; set; }
+
+        public Roles Role { get; set; }
+    }
+}
