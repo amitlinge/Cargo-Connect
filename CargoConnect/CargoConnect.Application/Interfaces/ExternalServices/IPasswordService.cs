c16632e46177a0dc8122df4294db79bfc7c87c08
amitkumar linge <amitlinge410@gmail.com>
2026-04-23 15:46:13 +0530

    password service interface

---
+++ CargoConnect/CargoConnect.Application/Interfaces/ExternalServices/IPasswordService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoConnect.Application.Interfaces.ExternalServices
+{
+    public interface IPasswordService
+    {
+        string HashPassword(string email,string password);
+        bool VerifyPassword(string email, string userPassword,string storedPassword);
+    }
+}
