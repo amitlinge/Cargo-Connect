340d438263e3ec3d751db6dc3d46f278e6b8799c
Ankita Khade <ankita24.net@gmail.com>
2026-04-23 13:14:18 +0530

    Interface For BookingService

---
+++ CargoConnect/CargoConnect.Application/Interfaces/Services/IBookingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CargoConnect.Application.DTOs.Booking;
+
+namespace CargoConnect.Application.Interfaces.Services
+{
+    public interface IBookingService
+    {
+        Task<List<BookingViewDTO>> GetAllAsync();
+        Task<BookingViewDTO> GetByIdAsync(Guid id);
+
+        Task<bool> CreateAsync(BookingCreateDTO bookingCreateDto);
+        Task<bool> UpdateAsync(BookingUpdateDTO bookingUpdateDto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
