61e860267ac41f20ce8ac1dddbccb88cfcc942de
jaysh <jayshreethorat17@gmail.com>
2026-04-23 16:53:41 +0530

    Issue-#20

---
+++ CargoConnect/CargoConnect.Application/Interfaces/Services/ITrackingService.cs
@@ -0,0 +1,23 @@
+﻿using CargoConnect.Application.DTOs.Tracking;
+using CargoConnect.Application.DTOs.Transaction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoConnect.Application.Interfaces.Services
+{
+    public interface ITrackingService
+    {
+
+        Task<List<TrackingViewDTO>> GetAllAsync();
+
+        Task<TrackingViewDTO> GetByIdAsync(Guid id);
+
+        Task<bool> CreateAsync(TrackingCreateDTO trackingCreateDto);
+
+        Task<bool> DeleteAsync(Guid id);
+
+    }
+}
