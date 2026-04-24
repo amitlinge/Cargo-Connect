ef2d7cbb3f6620d8f479728d982859259b9b6bb8
jaysh <jayshreethorat17@gmail.com>
2026-04-24 22:51:30 +0530

    Create ITrackingService

---
+++ CargoConnect/CargoConnect.Application/Interfaces/ITrackingService.cs
@@ -0,0 +1,19 @@
+﻿using CargoConnect.Application.DTOs.Tracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CargoConnect.Application.Interfaces
+{
+    public interface ITrackingService
+    {
+        Task<List<TrackingViewDTO>> GetAllAsync();
+        Task<TrackingViewDTO> GetByIdAsync(Guid id);
+        Task<bool> UpdateAsync(TrackingUpdateDTO trackingUpdateDto);
+        Task<bool> DeleteAsync(Guid id);
+
+
+    }
+}
