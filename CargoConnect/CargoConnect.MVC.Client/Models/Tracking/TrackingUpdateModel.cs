c08c79c11441a80dd73f2a6bef2bbe9652faa34f
jaysh <jayshreethorat17@gmail.com>
2026-04-24 23:09:17 +0530

    Updated Traking Model

---
+++ CargoConnect/CargoConnect.MVC.Client/Models/Tracking/TrackingUpdateModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CargoConnect.MVC.Client.Models.Tracking
+{
+    public class TrackingUpdateModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid BookingId { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+    }
+}
+
