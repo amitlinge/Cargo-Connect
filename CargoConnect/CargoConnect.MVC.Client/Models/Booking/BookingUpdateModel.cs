9d5033eabe5dea1fec71b8acd1dac67c4eb4cf5b
Ankita Khade <ankita24.net@gmail.com>
2026-04-24 10:36:25 +0530

    Update BookingModel

---
+++ CargoConnect/CargoConnect.MVC.Client/Models/Booking/BookingUpdateModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CargoConnect.Common.Enums;
+
+namespace CargoConnect.MVC.Client.Models.Booking
+{
+    public class BookingUpdateModel
+    {
+        [Required(ErrorMessage = "Status is required")]
+        [EnumDataType(typeof(BookingStatus), ErrorMessage = "Invalid status transition")]
+        public BookingStatus Status { get; set; } = BookingStatus.Pending;
+    }
+}
