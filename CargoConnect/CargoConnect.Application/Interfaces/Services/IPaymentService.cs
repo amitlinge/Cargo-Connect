d6524c5b994f6f21c9101aca95fa28e91a78ed34
amitkumar linge <amitlinge410@gmail.com>
2026-04-25 20:44:45 +0530

    Payment service interface

---
+++ CargoConnect/CargoConnect.Application/Interfaces/Services/IPaymentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CargoConnect.Application.DTOs.Payment;
+
+namespace CargoConnect.Application.Interfaces.Services
+{
+    public interface IPaymentService
+    {
+        Task<List<PaymentViewDTO>> GetAllAsync();
+        Task<PaymentViewDTO> GetByIdAsync(Guid id);
+        Task<bool> CreateAsync(PaymentCreateDTO paymentCreateDTO);
+        Task<bool> UpdateAsync(PaymentUpdateDTO paymentUpdateDTO);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
